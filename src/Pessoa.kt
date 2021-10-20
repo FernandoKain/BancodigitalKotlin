647a446e5624028c44bae63b88e8ade4c1db13a5
FernandoKain <fernandomanga@hotmail.com>
2021-10-20 14:45:19 -0300

    Primeiro Commit do Projeto de um Banco Digital

---
+++ src/Pessoa.kt
@@ -0,0 +1,11 @@
+class Pessoa {
+    var nome: String = "Jether"
+    var cpf: String = "123.123.123-24"
+}
+
+fun main(){
+    val jether = Pessoa()
+
+    print(jether.nome)
+    print(jether.cpf)
+}
